eb2566907130e6a8f09c0356ab9d3cabd5847499
Justin Peters <JMarvel@gmail.com>
2022-06-10 09:01:47 -0700

    add function and loop to count up from 0 to specified number

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Utility Logic//
+
+//Business Logic//
+
+function numberList(numberInput) {
+  let numArray = [];
+  for (let i = 0; i <= numberInput; i +=1) {
+    numArray.push(i);
+  }
+  return numArray;
+}
+
+
+//UI Logic//
