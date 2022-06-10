13e2cabc513b4462124f2c416bdb80b79fe60fb0
Justin Peters <JMarvel@gmail.com>
2022-06-10 14:36:03 -0700

    add please enter number if no number entered and not a number if something other that a number is entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 //Utility Logic//
+function noInput(numberInput) {
+  return ((numberInput.length===0));
+}
 
+function notNumber(numberInput) {
+  if (isNaN(numberInput)) {
+    return 'Not a Number!';
+  }
+}
 //Business Logic//
 
 function numberList(numberInput) {
   let numReturn = [];
+  if (noInput(numberInput)) {
+    return "Please enter a number!";
+  }
+  if (notNumber(numberInput)) {
+    return "Not a Number!";
+  }
   for (let i = 0; i <= numberInput; i +=1) {
     numReturn.push(i);
   }
